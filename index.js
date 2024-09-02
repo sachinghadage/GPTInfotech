894263b60603aee0cfb5f8fbe3c49f4b913747f0
Sachin Ghadage <sachinghadage232002@gmail.com>
2024-09-02 21:00:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+window.onscroll = function() {stickyNavbar()};
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+const carousel = document.getElementById('carousel');
+    const slides = carousel.children;
+    const totalSlides = slides.length;
+    let index = 0;
+
+    function showSlide(i) {
+      carousel.style.transform = `translateX(-${i * 100}%)`;
+    }
+
+    document.getElementById('next').addEventListener('click', () => {
+      index = (index + 1) % totalSlides;
+      showSlide(index);
+    });
+
+    document.getElementById('prev').addEventListener('click', () => {
+      index = (index - 1 + totalSlides) % totalSlides;
+      showSlide(index);
+    });
+
+    // Auto slide
+    setInterval(() => {
+      index = (index + 1) % totalSlides;
+      showSlide(index);
+    }, 6000); // Change every 3 seconds
