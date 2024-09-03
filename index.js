cec1dd3814d43f44f2a1062cf73260b6b98e8677
Sachin Ghadage <sachinghadage232002@gmail.com>
2024-09-03 17:09:41 +0530

    updated navbar animation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-window.onscroll = function() {stickyNavbar()};
+window.onscroll = function() {
+  stickyNavbar();
+};
 
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar-custom");
 const sticky = navbar.offsetTop;
 
 function stickyNavbar() {
@@ -12,26 +14,26 @@ function stickyNavbar() {
 }
 
 const carousel = document.getElementById('carousel');
-    const slides = carousel.children;
-    const totalSlides = slides.length;
-    let index = 0;
+const slides = carousel.children;
+const totalSlides = slides.length;
+let index = 0;
 
-    function showSlide(i) {
-      carousel.style.transform = `translateX(-${i * 100}%)`;
-    }
+function showSlide(i) {
+  carousel.style.transform = `translateX(-${i * 100}%)`;
+}
 
-    document.getElementById('next').addEventListener('click', () => {
-      index = (index + 1) % totalSlides;
-      showSlide(index);
-    });
+document.getElementById('next').addEventListener('click', () => {
+  index = (index + 1) % totalSlides;
+  showSlide(index);
+});
 
-    document.getElementById('prev').addEventListener('click', () => {
-      index = (index - 1 + totalSlides) % totalSlides;
-      showSlide(index);
-    });
+document.getElementById('prev').addEventListener('click', () => {
+  index = (index - 1 + totalSlides) % totalSlides;
+  showSlide(index);
+});
 
-    // Auto slide
-    setInterval(() => {
-      index = (index + 1) % totalSlides;
-      showSlide(index);
-    }, 6000); // Change every 3 seconds
+// Auto slide
+setInterval(() => {
+  index = (index + 1) % totalSlides;
+  showSlide(index);
+}, 6000); // Change every 6 seconds
